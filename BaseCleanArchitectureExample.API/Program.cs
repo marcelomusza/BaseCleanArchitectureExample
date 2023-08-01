a67a31f3fa928e2be2d0195d7429616d3421e53c
Marcelo Muszalski <marcelo.muszalski@dcsl.com>
2023-08-01 12:34:23 +0200

    Initial commit with the exercise

---
+++ BaseCleanArchitectureExample.API/Program.cs
@@ -0,0 +1,37 @@
+using BaseCleanArchitectureExample.Application;
+using BaseCleanArchitectureExample.Infrastructure;
+using BaseCleanArchitectureExample.Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BaseCleanArchitectureExample.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BaseCleanArchitectureExample.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
